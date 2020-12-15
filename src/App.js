3e3c626060d80b1b726d4351fbe1dd352f2b43b5
Daniel Floyd <gleek.dan@gmail.com>
2020-12-15 15:18:08 -0700

    Chapter 14 - Working with Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 // import global styles
 import GlobalStyle from './components/GlobalStyle';
 // Import routes
 import Pages from './pages';
 
+// configure our API URI & cache
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+// configure Apollo Client
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true
+});
+
 const App = () => {
   return (
-    <div>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Pages />
-    </div>
+    </ApolloProvider>
   );
 };
 
